435084e7dc8160e2e0f9d12905992f048a95f0d8
Peter Milovcik <peter.milovcik@gmail.com>
2019-07-19 10:12:29 +0200

    Refactoring

---
+++ MenoPreDieta/MenoPreDieta/Core/Pair.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MenoPreDieta.Core
+{
+    public class Pair : IEquatable<Pair>
+    {
+        public Pair(int id, Name first, Name second)
+        {
+            Id = id;
+            First = first;
+            Second = second;
+        }
+
+        public int Id { get; }
+
+        public Name First { get; }
+
+        public Name Second { get; }
+
+        public Pick PickFirst() => new Pick(Id, this, First);
+
+        public Pick PickSecond() => new Pick(Id, this, Second);
+
+        public bool Equals(Pair other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id && Equals(First, other.First) && Equals(Second, other.Second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Pair) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id;
+                hashCode = (hashCode * 397) ^ (First != null ? First.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Second != null ? Second.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Pair left, Pair right) => Equals(left, right);
+
+        public static bool operator !=(Pair left, Pair right) => !Equals(left, right);
+    }
+}
---
+++ MenoPreDieta/MenoPreDieta/Core/Pick.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MenoPreDieta.Core
+{
+    public class Pick : IEquatable<Pick>
+    {
+        public Pick(int id, Pair pair, Name picked)
+        {
+            Id = id;
+            Pair = pair;
+            Picked = picked;
+        }
+
+        public int Id { get; }
+
+        public Pair Pair { get; }
+
+        public Name Picked { get; }
+
+        public bool Equals(Pick other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id && Equals(Pair, other.Pair) && Equals(Picked, other.Picked);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Pick) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id;
+                hashCode = (hashCode * 397) ^ (Pair != null ? Pair.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Picked != null ? Picked.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Pick left, Pick right) => Equals(left, right);
+
+        public static bool operator !=(Pick left, Pick right) => !Equals(left, right);
+    }
+}
